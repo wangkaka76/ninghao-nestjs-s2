9dfd4a311b08e202eec12bdd26b9573bd2b04fc4
王丽徽 <708982444@qq.com>
2020-01-29 12:09:06 +0800

    s2-06-01-02-create-microservice

--- apps/nest-1/src/main.ts
+++ apps/nest-1/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { from } from 'rxjs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 5555,
+    }
+  });
+  await app.listen(() => console.log('nest-1 service has started.'));
 }
 bootstrap();
