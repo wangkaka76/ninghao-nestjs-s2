83966e138d15f43bb3c9f27a806106d535402e25
王丽徽 <708982444@qq.com>
2020-01-30 17:31:52 +0800

    s2-06-01-04-client-proxy

--- apps/ninghao-nest/src/app.controller.ts
+++ apps/ninghao-nest/src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+    @Inject('NEST_1') private readonly client: ClientProxy,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): Observable<string> {
+    return this.client.send('greet', 'ninghao');
   }
 }
--- apps/ninghao-nest/src/app.module.ts
+++ apps/ninghao-nest/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule.register([
+    {
+      name: 'NEST_1',
+      transport: Transport.TCP,
+      options: {
+        host: 'localhost',
+        port: 5555,
+      },
+    },
+  ]),
+],
   controllers: [AppController],
   providers: [AppService],
 })
